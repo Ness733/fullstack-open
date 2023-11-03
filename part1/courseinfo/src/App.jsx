6649c9ad98bb0c1fd764c525b7477de07cea3852
Nestor Rosales <rosales.nestorr@gmail.com>
2023-11-03 18:49:05 -0300

    courseinfo folder with activity

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,57 @@
+function App() {
+	const course = "Half Stack Application Development";
+	const part1 = "Fundamentals of React";
+	const exercises1 = 10;
+	const part2 = "Using props to pass data";
+	const exercises2 = 7;
+	const part3 = "State of a component";
+	const exercises3 = 14;
+
+	const Header = (props) => {
+		return (
+			<header>
+				<h1>{props.course}</h1>
+			</header>
+		);
+	};
+
+	const Part = (props) => {
+		return (
+			<div>
+				<p>
+					{props.part} {props.exercises}
+				</p>
+			</div>
+		);
+	};
+
+	const Content = () => {
+		return (
+			<div>
+				<Part part={part1} exercises={exercises1} />
+				<Part part={part2} exercises={exercises2} />
+				<Part part={part3} exercises={exercises3} />
+			</div>
+		);
+	};
+
+	const Total = () => {
+		return (
+			<div>
+				<p>
+					Number of exercises {exercises1 + exercises2 + exercises3}
+				</p>
+			</div>
+		);
+	};
+
+	return (
+		<>
+			<Header course={course} />
+			<Content />
+			<Total />
+		</>
+	);
+}
+
+export default App;
